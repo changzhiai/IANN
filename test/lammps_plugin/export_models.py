f32e799453186de18c1fab131a94e3da31b01094
Changzhi Ai <changzhiai@outlook.com>
2025-05-20 15:51:03 -0700

    Update export models

--- test/lammps_plugin/export_models.py
+++ test/lammps_plugin/export_models.py
@@ -3,13 +3,17 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+convert_model_for_lammps(model_path='test/equiformerV2/model_output/best_model.pth', 
+                         model_type='equiformerV2', 
+                         output_path='test/lammps_plugin/export_equiformerV2.pth')
+
 # convert_model_for_lammps(model_path='test/mace/model_output/best_model.pth', 
 #                          model_type='mace', 
 #                          output_path='test/lammps_plugin/export_mace.pth')
 
-convert_model_for_lammps(model_path='test/nequip/model_output/best_model.pth', 
-                         model_type='nequip', 
-                         output_path='test/lammps_plugin/export_nequip.pth')
+# convert_model_for_lammps(model_path='test/nequip/model_output/best_model.pth', 
+#                          model_type='nequip', 
+#                          output_path='test/lammps_plugin/export_nequip.pth')
 
 # convert_model_for_lammps(model_path='test/painn/model_output/best_model.pth', 
 #                          model_type='painn', 
