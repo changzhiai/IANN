84b844556d01e4d86b9cb0ed21cc2c5b16a842df
Changzhi Ai <changzhiai@outlook.com>
2025-05-22 10:49:36 -0700

    Update train

--- test/painn/train.py
+++ test/painn/train.py
@@ -6,7 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 trainer = Trainer(
     model="painn",
-    config={"device": "cpu", 
+    config={"device": "cpu",
+            "node_size": 128,
             'output_dir': 'test/painn/model_output',
             "log_interval": 1, },
     distributed=False
