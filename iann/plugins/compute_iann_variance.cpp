20ec03d86dada876f019371031ef7cfa58fca5fc
Changzhi Ai <changzhiai@outlook.com>
2025-05-30 16:52:36 -0700

    Update variance

--- iann/plugins/compute_iann_variance.cpp
+++ iann/plugins/compute_iann_variance.cpp
@@ -1,14 +1,25 @@
 /* ----------------------------------------------------------------------
-   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   https://www.lammps.org/, Sandia National Laboratories
-   LAMMPS development team: developers@lammps.org
+   Custom thermo style for IANN potential
 
-   Copyright (2003) Sandia Corporation.  Under the terms of Contract
-   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under
-   the GNU General Public License.
+   There are 4 components to the variance:
+   - energy_variance
+   - force_variance
+   - max_energy_variance
+   - max_force_variance
+   
+   # Usage: 
+   
+   # Set up compute for variance
+   compute variance all iann/variance
 
-   See the README file in the top-level LAMMPS directory.
+   # Use in thermo output
+   thermo_style custom step pe ke etotal press c_variance[1] c_variance[2] c_variance[3] c_variance[4]
+
+   thermo_modify colname c_variance[1] energy_variance
+   thermo_modify colname c_variance[2] force_variance
+   thermo_modify colname c_variance[3] max_energy_variance
+   thermo_modify colname c_variance[4] max_force_variance
+
 ------------------------------------------------------------------------- */
 
 #include "compute_iann_variance.h"
