bf4f5519ca792204563bd94dca8910faacac21a2
Changzhi Ai <changzhiai@outlook.com>
2025-07-25 18:46:34 -0700

    define modules

--- iann/data/__init__.py
+++ iann/data/__init__.py
@@ -1,1 +1,3 @@
 from .data import AtomsData, replace_properties, AseDataReader, AseDataset, collate_atomsdata
+
+__all__ = ["AtomsData", "replace_properties", "AseDataReader", "AseDataset", "collate_atomsdata"]
---
+++ iann/plugins/__init__.py
@@ -0,0 +1,3 @@
+from .converter import LAMMPSModelWrapper, EnsembleLAMMPSModelWrapper, convert_model_for_lammps, convert_models_for_lammps
+
+__all__ = ["LAMMPSModelWrapper", "EnsembleLAMMPSModelWrapper", "convert_model_for_lammps", "convert_models_for_lammps"]
