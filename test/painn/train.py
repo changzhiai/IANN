d2e4e8e94a84aac1c1db96cfc3316390b8b7b6a0
Changzhi Ai <changzhiai@outlook.com>
2025-09-10 12:13:33 -0700

    Update examples

--- test/painn/train.py
+++ test/painn/train.py
@@ -1,4 +1,4 @@
-from iann.trainer.trainer import Trainer
+from iann.trainer import Trainer
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -7,9 +7,16 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 trainer = Trainer(
     model="painn",
     config={"device": "cpu",
-            "node_size": 128,
-            'output_dir': 'test/painn/model_output',
-            "log_interval": 1, },
+            "num_channels": 64,
+            "num_layers": 3,
+            'output_dir': 'test/painn/output',
+            # 'load_model': 'test/painn/output/model.pt',
+            "log_interval": 1,
+            "learning_rate": 0.0001,
+            "stop_patience": 400,
+            'forces_weight': 0.999,
+            },
     distributed=False
     )
-trainer.train("test/Pt_ads.traj")
+# trainer.train("test/Pt_ads.traj")
+trainer.train("test/dft_PdTiH_adss_r0_to_r31_final_tot.traj")
